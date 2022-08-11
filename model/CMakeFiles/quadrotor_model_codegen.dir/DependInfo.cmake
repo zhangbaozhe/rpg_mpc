d0fa2ac62ef2e12843ab808087c2b9fcfd6059c4
Baozhe Zhang <907711755@qq.com>
2022-08-11 16:25:29 +0800

    modified CMakeList

---
+++ model/CMakeFiles/quadrotor_model_codegen.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/rpg_mpc/model/quadrotor_model_thrustrates.cpp" "/home/baozhe/FastLab/landing_ws_rewrite/src/rpg_mpc/model/CMakeFiles/quadrotor_model_codegen.dir/quadrotor_model_thrustrates.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/ACADOtoolkit"
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/ACADOtoolkit/acado"
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/ACADOtoolkit/external_packages"
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/ACADOtoolkit/external_packages/qpOASES-3.2.0/include"
+  "/home/baozhe/FastLab/landing_ws_rewrite/src/ACADOtoolkit/build"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ model/CMakeFiles/quadrotor_model_codegen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadrotor_model_codegen.dir/quadrotor_model_thrustrates.cpp.o"
+  "quadrotor_model_codegen"
+  "quadrotor_model_codegen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadrotor_model_codegen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
