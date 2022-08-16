e169bcf50da77be3c5afef88005e4c29287f1120
Baozhe Zhang <907711755@qq.com>
2022-08-16 09:34:08 +0800

    changed frame_id

--- src/mpc_controller.cpp
+++ src/mpc_controller.cpp
@@ -260,7 +260,7 @@ bool MpcController<T>::publishPrediction(
     ros::Time& time) {
   nav_msgs::Path path_msg;
   path_msg.header.stamp = time;
-  path_msg.header.frame_id = "map";
+  path_msg.header.frame_id = "base_link";
   geometry_msgs::PoseStamped pose;
   T dt = mpc_wrapper_.getTimestep();
 
